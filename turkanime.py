bf404715b37b8870bc9b30a8a1e86ae01b85cf26
Junicchi <junicchi@8chan.co>
2019-07-27 14:19:23 +0300

    gereksiz kütüphane importu silidi

--- turkanime.py
+++ turkanime.py
@@ -7,11 +7,10 @@
 """
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from time import sleep as delay
 from os import system as cmd
 
-driver = webdriver.Firefox()
+driver = webdriver.Firefox() # sürücüyü başlat
 
 HARICILER = [
 		"HDVID",
