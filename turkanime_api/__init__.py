e5980eed6a9dd4b26fcf81c07ba40925aae76f03
Junicchi <24779257+KebabLord@users.noreply.github.com>
2021-01-30 16:18:38 +0300

    init sistemi sadeleştirildi

--- turkanime_api/__init__.py
+++ turkanime_api/__init__.py
@@ -1,6 +1,10 @@
 from sys import exit as kapat
 import subprocess as sp
+from os import name
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 from .anime import AnimeSorgula,Anime
+from .players import elementi_bekle
 
 def gereksinim_kontrol():
     """ Gereksinimlerin erişilebilir olup olmadığını kontrol eder """
@@ -16,3 +20,27 @@ def gereksinim_kontrol():
     if eksik:
         print(stdout+"\nBelirtilen program yada programlar, program dizininde yada sistem PATH'ında bulunamadı. Lütfen klavuzdaki kurulum talimatlarını uygulayın.")
         kapat(1)
+
+def webdriver_hazirla():
+    """ Selenium webdriver'ı hazırla """
+    print(" "*50+"\rSürücü başlatılıyor...",end="\r")
+
+    options = Options()
+    options.add_argument('--headless')
+    profile = webdriver.FirefoxProfile()
+    profile.set_preference("dom.webdriver.enabled", False)
+    profile.set_preference('useAutomationExtension', False)
+    profile.set_preference('permissions.default.image', 2)
+    profile.set_preference("network.proxy.type", 0)
+    profile.update_preferences()
+    desired = webdriver.DesiredCapabilities.FIREFOX
+    if name == 'nt':
+        return webdriver.Firefox(
+            profile, options=options,service_log_path='NUL',
+            executable_path=r'geckodriver.exe', desired_capabilities=desired
+        )
+
+    return webdriver.Firefox(
+        profile, options=options,
+        service_log_path='/dev/null',desired_capabilities=desired
+    )
