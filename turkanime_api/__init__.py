1791312590188778077df10155a2b882d6848d48
Junicchi <24779257+KebabLord@users.noreply.github.com>
2023-07-20 02:51:46 +0300

    new structure init

--- turkanime_api/__init__.py
+++ turkanime_api/__init__.py
@@ -1,6 +1,1 @@
 """ TurkAnimu API """
-from .anime import AnimeSorgula,Anime
-from .players import elementi_bekle
-from .tools import gereksinim_kontrol,webdriver_hazirla,prompt_tema,clear,create_progress
-from .dosyalar import DosyaManager
-from .version import __author__,__version__,__build__,isGuncel,update_type
